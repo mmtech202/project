28ec19a1d288bc4b333c9dab21fa270d53de2757
mmtech202 <mmtech202@gmail.com>
2020-03-01 10:12:41 +0530

    redux setup

--- src/index.js
+++ src/index.js
@@ -1,31 +1,13 @@
-// import React from 'react';
-// import ReactDOM from 'react-dom';
-// import './index.css';
-// import App from './App';
-// import * as serviceWorker from './serviceWorker';
 
-// ReactDOM.render(<App />, document.getElementById('root'));
+import React from "react";
+import { render } from "react-dom";
+import { Provider } from "react-redux";
+import store from "./js/store/index";
+import App from "./js/components/App";
 
-// // If you want your app to work offline and load faster, you can change
-// // unregister() to register() below. Note this comes with some pitfalls.
-// // Learn more about service workers: https://bit.ly/CRA-PWA
-// serviceWorker.unregister();
-
-import React from 'react';
-import ReactDOM from 'react-dom';
-import {Provider} from 'react-redux';
-import {createStore, applyMiddleware} from 'redux';
-import reduxThunk from 'redux-thunk';
-import reducers from './reducers';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
-
-const store = createStore(reducers, {}, applyMiddleware(reduxThunk));
-
-ReactDOM.render(
+render(
   <Provider store={store}>
     <App />
   </Provider>,
   document.getElementById("root")
-);
-serviceWorker.register();
+  );
